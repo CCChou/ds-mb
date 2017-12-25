1c431049ba3b5a48711d8593063c4cf5dd4fedcc
CCChou <dses5900@gmail.com>
2017-12-25 21:18:48 +0800

    add hash method

--- source/ds-mb/src/main/java/ds/web/practice/util/SecureUtil.java
+++ source/ds-mb/src/main/java/ds/web/practice/util/SecureUtil.java
@@ -1,5 +1,17 @@
 package ds.web.practice.util;
 
-public class SecureUtil {
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
+import org.apache.commons.codec.binary.Hex;
+
+public class SecureUtil {    
+    public static String hash(String algorithm, String data) throws NoSuchAlgorithmException {
+        return hash(algorithm, data.getBytes());
+    }
+    public static String hash(String algorithm, byte[] data) throws NoSuchAlgorithmException {
+        MessageDigest md = MessageDigest.getInstance(algorithm);
+        md.update(data);
+        return new String(Hex.encodeHex(md.digest()));
+    }
 }
